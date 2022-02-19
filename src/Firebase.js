36a1bc823df049190f7119ca69a614533fc98494
Pedro Lucas <88797361+pelucs@users.noreply.github.com>
2022-02-18 23:54:41 -0300

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "AIzaSyAoMYKD-WoyWZQs6rHDAAP5CUAk5E1hFGQ",
-  authDomain: "netflix-clone-b731c.firebaseapp.com",
-  projectId: "netflix-clone-b731c",
-  storageBucket: "netflix-clone-b731c.appspot.com",
-  messagingSenderId: "135958462222",
-  appId: "1:135958462222:web:3ae2f9f83091ec2575e15f"
+  apiKey: "SUA CHAVE",
+  authDomain: "SUA CHAVE",
+  projectId: "SUA CHAVE",
+  storageBucket: "SUA CHAVE",
+  messagingSenderId: "SUA CHAVE",
+  appId: "SUA CHAVE"
 });
 
 const auth = firebase.auth();
